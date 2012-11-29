cc37134cac11eb5eecb1d35af99694b1bf393c03
Andrew Radev <andrey.radev@gmail.com>
2012-11-29 20:50:57 +0100

    Remove my ruby indent attempt
    Apparently, it's not particularly simple to indent ruby code. Next
    attempt should build on the exising ruby indent instead.

--- wip/my_ruby_indent/features/step_definitions/indent_steps.rb
+++
@@ -1,9 +0,0 @@
-Given /^my ruby indent is loaded$/ do
-  plugin_dir = File.expand_path('../../..', __FILE__)
-  @vim.add_plugin plugin_dir
-end
-
-When /^I reindent the file$/ do
-  @vim.normal 'gg=G'
-  @vim.write
-end
